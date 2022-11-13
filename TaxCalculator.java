3df5a2a426ba78c6c21890234820c2b9442ff404
kevinhnw <105079395+kevinhnw@users.noreply.github.com>
2022-11-12 21:30:10 -0800

    TaxCalculator V2
    #1 Prompts the user for their income. Income is needed for tax calculation.

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -3,6 +3,15 @@ import java.util.*;
 public class TaxCalculator {   
    
    public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+   
+      int income = getIncome(console);
+   
+   }
 
+   public static int getIncome(Scanner console) {
+      System.out.print("How much income do you have? Please type your answer here: ");
+      int income = console.nextInt();
+      return income;
    }
 }
