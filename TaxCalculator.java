ba6d10b5a3feab856040dd45b9916251b013d539
kevinhnw <105079395+kevinhnw@users.noreply.github.com>
2022-11-12 21:39:57 -0800

    TaxCalculator V4
    Fixes #1 Add comments.

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+// This program will prompt the user for their income and then calculate the amount of taxes 
+// they will need to pay.
+
 public class TaxCalculator {   
    
    public static void main(String[] args) {
@@ -9,6 +12,7 @@ public class TaxCalculator {
    
    }
 
+   // prompts the user for their income
    public static int getIncome(Scanner console) {
       System.out.print("How much income do you have? Please type your answer here: ");
       int income = console.nextInt();
