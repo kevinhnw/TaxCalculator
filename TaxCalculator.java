15ee0b196f3e3530cac62ce72143b7404ea91538
kevinhnw <105079395+kevinhnw@users.noreply.github.com>
2023-01-09 09:13:08 -0800

    TaxCalculator V5
    Added a class that prompts the user for filing status.

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -8,15 +8,25 @@ public class TaxCalculator {
    public static void main(String[] args) {
       Scanner console = new Scanner(System.in);
    
-      int income = getIncome(console);
+      double income = getIncome(console);
+      
+      String file = getFile(console);
    
    }
 
    // prompts the user for their income
-   public static int getIncome(Scanner console) {
+   public static double getIncome(Scanner console) {
       System.out.print("How much income do you have? Please type your answer here: ");
-      int income = console.nextInt();
+      double income = console.nextDouble();
       System.out.println("$" + income);
       return income;
+   }
+   
+   // prompts the user for filing status
+   public static String getFile(Scanner console) {
+      System.out.print("How are you filing? Single, Married Jointly, Married Separately, Head of Household? Please type your answer here: ");
+      String file = console.nextLine();
+      System.out.println(file);
+      return file;
    }
 }
